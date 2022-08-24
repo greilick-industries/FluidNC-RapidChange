0bebf71a9d6e2afb11d6cc291536bee0110c34ec
James Greilick <41349921+jgreilick@users.noreply.github.com>
2022-08-24 18:37:39 -0500

    Update FluidNC-3.4.4

---
+++ FluidNC/src/Custom/ATC.cpp
@@ -0,0 +1,39 @@
+#include "../GCode.h"
+#include "../Serial.h"
+#include "../Settings.h"
+#include "../Uart.h"
+#include "../WebUI/InputBuffer.h"
+
+void gc_exec_linef(const char* format, ...);
+
+void user_tool_change(uint8_t new_tool) {
+    if (new_tool == 1) {
+        gc_exec_linef("G20G90");
+        gc_exec_linef("G58");
+        gc_exec_linef("g1z4f100");
+        gc_exec_linef("x0y0");
+        gc_exec_linef("Z2");
+        gc_exec_linef("G54");
+        gc_exec_linef("s8000m3");
+        gc_exec_linef("g38.2f15z1.5");
+        gc_exec_linef("g0g91z-.05");
+        gc_exec_linef("g38.2f1z.2");
+        gc_exec_linef("g10l20p0z2.497");
+        gc_exec_linef("M5");
+        gc_exec_linef("g0g90z3");
+    
+        log_info("Tool change completed");
+    }
+}
+
+// This comment is even newer
+void gc_exec_linef(const char* format, ...) {
+    Channel& r_channel = WebUI::inputBuffer;
+    va_list args;
+    char gc_line[20];
+    gc_line[strlen(format)] = '\r';
+    sprintf(gc_line, format, args);
+    Error line_executed = execute_line(gc_line, r_channel, WebUI::AuthenticationLevel::LEVEL_GUEST);
+    r_channel << ">" << gc_line << ":";
+    report_status_message(line_executed, r_channel);
+}
