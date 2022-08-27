0709305b944a32bbe4a59f49faa1dcde06c713dd
James Greilick <41349921+jgreilick@users.noreply.github.com>
2022-08-26 22:06:48 -0500

    Update ATC.cpp

--- FluidNC/src/Custom/ATC.cpp
+++ FluidNC/src/Custom/ATC.cpp
@@ -18,41 +18,47 @@ void machine_init() {
 
 }
 
+
+
 void user_tool_change(uint8_t new_tool) {
-    bool spindle_was_on = false;
-    bool was_incremental = false;
-    uint64_t spindle_spin_delay;
-    float saved_mpos[MAX_N_AXIS] = {};
+    // bool spindle_was_on = false;
+    // bool was_incremental = false;
+    // uint64_t spindle_spin_delay;
+    // float saved_mpos[MAX_N_AXIS] = {};
 
     // if (new_tool == current_tool) {
-    //     // TODO log
+    //     log_info("Existing tool requested.");
     //     return;
     // }
 
     // if (new_tool > TOOL_COUNT) {
-    //     // TODO log
+    //     log_info("Tool requested is out of range.");
     //     return;
     // }
 
     // protocol_buffer_synchronize();
     
-    convert_sys_mpos_to_array(saved_mpos);
+    // convert_sys_mpos_to_array(saved_mpos);
 
     // if (gc_state.modal.distance == Distance::Incremental) {
     //     was_incremental = true;
     //     gc_exec_linef(false, "G90");
     // }
 
-    if (gc_state.modal.spindle != SpindleState::Disable) {
-        spindle_was_on = true;
-        gc_exec_linef(false, "M5");
-        spindle_spin_delay = esp_timer_get_time() + (spindle->_spindown_ms * 1000);
+    // if (gc_state.modal.spindle != SpindleState::Disable) {
+    //     spindle_was_on = true;
+    //     gc_exec_linef(false, "M5");
 
-        uint64_t current_time = esp_timer_get_time();
-        if (current_time < spindle_spin_delay) {
-            vTaskDelay(spindle_spin_delay - current_time);
-        }
-    }
+
+    //     // spindle_spin_delay = esp_timer_get_time() + (spindle->_spindown_ms * 1000);
+
+    //     // uint64_t current_time = esp_timer_get_time();
+    //     // if (current_time < spindle_spin_delay) {
+    //     //     vTaskDelay(spindle_spin_delay - current_time);
+    //     // }
+    // }
+    Error test_error = Error::InvalidValue;
+    report_status_message(test_error, allChannels);
 }
 
 void convert_sys_mpos_to_array(float array_to_fill[MAX_N_AXIS]) {
@@ -64,14 +70,14 @@ void convert_sys_mpos_to_array(float array_to_fill[MAX_N_AXIS]) {
 
 // This comment is even newer
 void gc_exec_linef(bool sync_after, const char* format, ...) {
-    Channel& r_channel = WebUI::inputBuffer;
+    // Channel& r_channel = WebUI::inputBuffer;
     va_list args;
     char gc_line[20];
     gc_line[strlen(format)] = '\r';
     sprintf(gc_line, format, args);
-    Error line_executed = execute_line(gc_line, r_channel, WebUI::AuthenticationLevel::LEVEL_GUEST);
-    r_channel << ">" << gc_line << ":";
-    report_status_message(line_executed, r_channel);
+    Error line_executed = execute_line(gc_line, allChannels, WebUI::AuthenticationLevel::LEVEL_GUEST);
+    Uart0 << ">" << gc_line << ":";
+    report_status_message(line_executed, allChannels);
 }
 
 bool return_tool(uint8_t tool_num) {
